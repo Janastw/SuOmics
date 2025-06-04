b4e2c5adcb4ca0dcd89af91282f52e2e7fc09f4b
Thomas <janast@berkeley.edu>
2025-06-04 17:00:22 -0500

    Added a reference genome tag to the seurat object

--- scripts/generate_seurat_object.R
+++ scripts/generate_seurat_object.R
@@ -17,6 +17,18 @@ rna_counts <- data$`Gene Expression`
 atac_counts <- data$Peaks
 seurat_obj <- CreateSeuratObject(counts = data$`Gene Expression`, project = sample_name)
 
+# Labeling refence genome in seurat object - Defaults to mm10 if unable
+reference_genome <- "mm10"
+
+summary_filepath <- file.path(args[1], "outs", "summary.csv")
+tryCatch({
+  genome_name <- read.csv(summary_filepath, stringsAsFactors = FALSE)$Genome[1]
+}, error = function(e) {
+  message("summary.csv or reference genome not found — defaulting to 'mm10'")
+})
+
+seurat_obj@misc$reference_genome <- reference_genome
+
 # Calculate percentage of RNA reads mapped to mitochondrial genes - indicator for stress/damaged/lysed cells for exclusion
 seurat_obj[["percent.mt"]] <- PercentageFeatureSet(seurat_obj, pattern = "^mt-")
 
